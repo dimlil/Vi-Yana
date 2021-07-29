9686c5ae746bc34fb4a240f634191d84d5551fc5
dimlil <d.v.vassilev@gmail.com>
2021-07-29 04:16:25 +0300

    feat/ created home page background

--- src/Pages/HomePage/Home.js
+++ src/Pages/HomePage/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from './Home.module.css'
 
 export default function Home() {
     return (
-        <div>
+        <div className={styles.mainDiv}>
             <p>Helloo</p>
         </div>
     )
