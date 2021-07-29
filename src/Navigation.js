a4e35d7d9caf7e5c1b1fbfe44502b26455f0761b
dimlil <d.v.vassilev@gmail.com>
2021-07-29 03:28:51 +0300

    feat/ added home page

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './Pages/HomePage/Home'
 export default function Navigation() {
     return (
         <div>
-
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                </Switch>
+            </BrowserRouter>
         </div>
     )
 }
