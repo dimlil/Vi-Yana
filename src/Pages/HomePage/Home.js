d350bec1620513a4242a715847f6eb53ed2b09ca
dimlil <d.v.vassilev@gmail.com>
2021-08-30 22:03:38 +0300

    feat/ created folder for component

--- src/Pages/HomePage/Home.js
+++ src/Pages/HomePage/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './Home.module.css'
-import HomePageNavigationButtons from '../../Components/HomePageNavigationButtons'
+import HomePageNavigationButtons from '../../Components/navigationButtons/HomePageNavigationButtons'
 
 export default function Home() {
     return (
