9e4ac0b50e2be6129b32eaf865ba71a6ba000bc5
dimlil <d.v.vassilev@gmail.com>
2021-09-03 23:37:03 +0300

    fix/ semantic errors

--- src/Pages/HomePage/Home.js
+++ src/Pages/HomePage/Home.js
@@ -1,25 +1,29 @@
-import React from 'react'
-import styles from './Home.module.css'
-import HomePageNavigationButtons from '../../Components/navigationButtons/HomePageNavigationButtons'
-import Logo from '../../Components/logo/Logo'
-import AboutUs from '../../Components/aboutUs/AboutUs'
+import React from "react";
+import styles from "./Home.module.css";
+import HomePageNavigationButtons from "../../Components/navigationButtons/HomePageNavigationButtons";
+import Logo from "../../Components/logo/Logo";
+import AboutUs from "../../Components/aboutUs/AboutUs";
 
 export default function Home() {
-    return (
-        <div className={styles.mainDiv}>
-            <div className={styles.logoDiv}>
-                {/* <p className={styles.viyana}>ВиЯна</p> */}
-                <Logo />
-                <p className={styles.moto}>Детски терапевтичен и игрови център</p>
-                <div className={styles.linkswrapper}>
-                    {/* <HomePageNavigationButtons valueFromParent={'Информация'} /> */}
-                    <HomePageNavigationButtons valueFromParent={'Услуги'} />
-                    <HomePageNavigationButtons valueFromParent={'Защо ранната интервенция е важна?'} />
-                    <HomePageNavigationButtons valueFromParent={'Полезно'} />
-                    <HomePageNavigationButtons valueFromParent={'Контакти'} />
-                </div>
-            </div>
-            <AboutUs />
+  return (
+    <div>
+      <header className={styles.mainDiv}>
+        <div className={styles.logoDiv}>
+          {/* <p className={styles.viyana}>ВиЯна</p> */}
+          <Logo size="100px" />
+          <p className={styles.moto}>Детски терапевтичен и игрови център</p>
+          <div className={styles.linkswrapper}>
+            {/* <HomePageNavigationButtons valueFromParent={'Информация'} /> */}
+            <HomePageNavigationButtons valueFromParent={"Услуги"} />
+            <HomePageNavigationButtons
+              valueFromParent={"Защо ранната интервенция е важна?"}
+            />
+            <HomePageNavigationButtons valueFromParent={"Полезно"} />
+            <HomePageNavigationButtons valueFromParent={"Контакти"} />
+          </div>
         </div>
-    )
+      </header>
+      <AboutUs />
+    </div>
+  );
 }
