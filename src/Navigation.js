6f9ab754a6196420b57a16214bc8ab65d6e65b86
dimlil <d.v.vassilev@gmail.com>
2021-09-04 00:06:43 +0300

    feat/ added service page

--- src/Navigation.js
+++ src/Navigation.js
@@ -3,13 +3,14 @@ import './Navigation.module.css'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './Pages/HomePage/Home'
 import Footer from './Components/footer/Footer'
+import ServicesPage from './Pages/ServicesPage/ServicesPage'
 export default function Navigation() {
     return (
         <div>
             <BrowserRouter>
                 <Switch>
                     <Route path="/" exact component={Home} />
-                    <Route path="/services" component={Home} />
+                    <Route path="/services" component={ServicesPage} />
                     <Route path="/Why-is-early-intervention-important" component={Home} />
                     <Route path="/useful" component={Home} />
                     <Route path="/contacts" component={Home} />
---
+++ src/Pages/ServicesPage/ServicesPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ServicesPage() {
+    return (
+        <div>
+            <p>Service</p>
+        </div>
+    )
+}
