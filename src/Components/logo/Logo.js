597f780f4f6fe1c89c32d8db0bdce265bf7ec7e1
dimlil <d.v.vassilev@gmail.com>
2021-09-03 23:10:20 +0300

    fix/ added prop for size of logo and make the component reusable

--- src/Components/logo/Logo.js
+++ src/Components/logo/Logo.js
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "./logo.module.css";
 
-export default function Logo() {
+export default function Logo({size}) {
   return (
     <div>
-      <p className={styles.logoText}>
+      <p className={styles.logoText} style={{fontSize: size}}>
         <span className={styles.Span0}>В</span>
         <span className={styles.Span1}>и</span>
         <span className={styles.Span2}>Я</span>
