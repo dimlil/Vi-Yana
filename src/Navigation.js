f3307b3678c3cf18d1240ff2e859c0b09d4ec7a4
dimlil <d.v.vassilev@gmail.com>
2021-09-10 00:52:53 +0300

    fix/ formated the code

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,22 +1,23 @@
-import React from 'react'
-import './Navigation.module.css'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Home from './Pages/HomePage/Home'
-import Footer from './Components/footer/Footer'
-import ServicesPage from './Pages/ServicesPage/ServicesPage'
+import React from "react";
+import "./Navigation.module.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./Pages/HomePage/Home";
+import Footer from "./Components/footer/Footer";
+import ServicesPage from "./Pages/ServicesPage/ServicesPage";
+
 export default function Navigation() {
-    return (
-        <div>
-            <BrowserRouter>
-                <Switch>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/services" component={ServicesPage} />
-                    <Route path="/Why-is-early-intervention-important" component={Home} />
-                    <Route path="/useful" component={Home} />
-                    <Route path="/contacts" component={Home} />
-                </Switch>
-            </BrowserRouter>
-            <Footer />
-        </div>
-    )
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/services" component={ServicesPage} />
+          <Route path="/Why-is-early-intervention-important" component={Home} />
+          <Route path="/useful" component={Home} />
+          <Route path="/contacts" component={Home} />
+        </Switch>
+      </BrowserRouter>
+      <Footer />
+    </div>
+  );
 }
