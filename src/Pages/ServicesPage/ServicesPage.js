ef600a68829095d5a97917d049a1ca05affb28a0
dimlil <d.v.vassilev@gmail.com>
2021-09-10 00:53:26 +0300

    feat/ added basic header

--- src/Pages/ServicesPage/ServicesPage.js
+++ src/Pages/ServicesPage/ServicesPage.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from '../../Components/header/Header'
 
 export default function ServicesPage() {
     return (
         <div>
+            <Header />
             <p>Service</p>
         </div>
     )
