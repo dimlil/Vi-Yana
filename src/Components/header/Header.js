56d413d685ffbbb1539d575b649e44004af5d99c
dimlil <d.v.vassilev@gmail.com>
2021-09-28 23:12:03 +0300

    fix/ separate code using components

--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -1,39 +1,12 @@
 import React from "react";
-import Logo from "../logo/Logo";
-import styles from "./Header.module.css";
-import { Link } from "react-router-dom";
-import HomePageNavigationButtons from "../navigationButtons/HomePageNavigationButtons";
 import HeaderForAllComponentsExeptHome from "./HeaderForAllComponentsExeptHome";
+import HeaderForHome from "./HeaderForHome";
 
 export default function Header() {
   let location = window.location.pathname;
   return (
     <div>
-       {location == "/" ? <div className={styles.logoDiv}>
-      
-      {/* <p className={styles.viyana}>ВиЯна</p> */}
-      <Logo size="100px" position="center"/>
-      <p className={styles.moto}>Детски терапевтичен и игрови център</p>
-      <div className={styles.linkswrapper}>
-        {/* <HomePageNavigationButtons valueFromParent={'Информация'} /> */}
-        <HomePageNavigationButtons
-          valueFromParent={"Услуги"}
-          link={"/services"}  parentName={'home'}
-        />
-        <HomePageNavigationButtons
-          valueFromParent={"Защо ранната интервенция е важна?"}
-          link={"/Why-is-early-intervention-important"} parentName={'home'}
-        />
-        <HomePageNavigationButtons
-          valueFromParent={"Полезно"} parentName={'home'}
-          link={"/useful"}
-        />
-        <HomePageNavigationButtons
-          valueFromParent={"Контакти"} parentName={'home'}
-          link={"/contacts"}
-        />
-      </div>
-    </div> : <HeaderForAllComponentsExeptHome />}
+       { location == "/" ? <HeaderForHome /> : <HeaderForAllComponentsExeptHome /> }
     </div>
   );
 }
