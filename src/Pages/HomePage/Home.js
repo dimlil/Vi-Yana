269e2655c5ad4f19bd1181401ec7e1e9521d4b56
dimlil <d.v.vassilev@gmail.com>
2021-09-28 22:53:18 +0300

    fix/ remove hardcoded header and add header commponent

--- src/Pages/HomePage/Home.js
+++ src/Pages/HomePage/Home.js
@@ -3,35 +3,13 @@ import styles from "./Home.module.css";
 import HomePageNavigationButtons from "../../Components/navigationButtons/HomePageNavigationButtons";
 import Logo from "../../Components/logo/Logo";
 import AboutUs from "../../Components/aboutUs/AboutUs";
+import Header from "../../Components/header/Header";
 
 export default function Home() {
   return (
     <div>
       <header className={styles.mainDiv}>
-        <div className={styles.logoDiv}>
-          {/* <p className={styles.viyana}>ВиЯна</p> */}
-          <Logo size="100px" />
-          <p className={styles.moto}>Детски терапевтичен и игрови център</p>
-          <div className={styles.linkswrapper}>
-            {/* <HomePageNavigationButtons valueFromParent={'Информация'} /> */}
-            <HomePageNavigationButtons
-              valueFromParent={"Услуги"}
-              link={"/services"}
-            />
-            <HomePageNavigationButtons
-              valueFromParent={"Защо ранната интервенция е важна?"}
-              link={"/Why-is-early-intervention-important"}
-            />
-            <HomePageNavigationButtons
-              valueFromParent={"Полезно"}
-              link={"/useful"}
-            />
-            <HomePageNavigationButtons
-              valueFromParent={"Контакти"}
-              link={"/contacts"}
-            />
-          </div>
-        </div>
+        <Header />
       </header>
       <AboutUs />
     </div>
