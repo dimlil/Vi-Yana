ada87078ec83af77ea785f7e408a49bdff098440
dimlil <d.v.vassilev@gmail.com>
2021-09-28 22:57:24 +0300

    fix/ made component more reusable

--- src/Components/logo/Logo.js
+++ src/Components/logo/Logo.js
@@ -1,10 +1,10 @@
 import React from "react";
 import styles from "./logo.module.css";
 
-export default function Logo({size}) {
+export default function Logo({size,position,marginTop}) {
   return (
     <div>
-      <p className={styles.logoText} style={{fontSize: size}}>
+      <p className={styles.logoText} style={{fontSize: size, textAlign:position, marginTop: marginTop}}>
         <span className={styles.Span0}>В</span>
         <span className={styles.Span1}>и</span>
         <span className={styles.Span2}>Я</span>
