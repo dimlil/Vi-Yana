3ce62d5bc230fe09d338b278f1d62ba34ce5eb41
dimlil <d.v.vassilev@gmail.com>
2021-09-28 22:54:33 +0300

    fix/ made component more reusable

--- src/Components/navigationButtons/HomePageNavigationButtons.js
+++ src/Components/navigationButtons/HomePageNavigationButtons.js
@@ -1,11 +1,24 @@
 import React from "react";
 import styles from "./linksstyle.module.css";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { Fragment } from "react/cjs/react.production.min";
 
-export default function HomePageNavigationButtons({ valueFromParent, link }) {
+export default function HomePageNavigationButtons({
+  valueFromParent,
+  link,
+  parentName,
+}) {
   return (
-    <Link to={link} className={styles.linkWrapper}>
-      <p className={styles.links}>{valueFromParent}</p>
-    </Link>
+    <Fragment>
+      {parentName == "home" ? (
+        <Link to={link} className={styles.linkWrapper}>
+          <p className={styles.links}>{valueFromParent}</p>
+        </Link>
+      ) : (
+        <Link to={link} className={styles.linkWrapperSecondView}>
+          <p className={styles.linksSecondView}>{valueFromParent}</p>
+        </Link>
+      )}
+    </Fragment>
   );
 }
