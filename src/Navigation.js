5981aacfd6e606593edaa48bdd8d888067d36df6
dimlil <d.v.vassilev@gmail.com>
2021-11-08 00:39:09 +0200

    feat/ added basic view of why is early intervention important

--- src/Navigation.js
+++ src/Navigation.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Pages/HomePage/Home";
 import Footer from "./Components/footer/Footer";
 import ServicesPage from "./Pages/ServicesPage/ServicesPage";
+import WhyIsEarlyInterventionImportant from "./Pages/WhyIsEarlyInterventionImportant/WhyIsEarlyInterventionImportantPage";
 
 export default function Navigation() {
   return (
@@ -12,7 +13,7 @@ export default function Navigation() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/services" component={ServicesPage} />
-          <Route path="/Why-is-early-intervention-important" component={Home} />
+          <Route path="/Why-is-early-intervention-important" component={WhyIsEarlyInterventionImportant} />
           <Route path="/useful" component={Home} />
           <Route path="/contacts" component={Home} />
         </Switch>
