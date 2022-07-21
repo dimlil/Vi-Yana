31a9e893af73b7f3d9b60e1da47779e0411b678e
dimlil <d.v.vassilev@gmail.com>
2022-07-21 14:44:25 +0300

    feat/ change logo

--- src/Components/logo/Logo.js
+++ src/Components/logo/Logo.js
@@ -1,16 +1,18 @@
 import React from "react";
 import styles from "./logo.module.css";
+import logo from './logo.png'
 
 export default function Logo({size,position,marginTop}) {
   return (
-    <div>
-      <p className={styles.logoText} style={{fontSize: size, textAlign:position, marginTop: marginTop}}>
+    <div className={styles.logoWrapper}>
+      {/* <p className={styles.logoText} style={{fontSize: size, textAlign:position, marginTop: marginTop}}>
         <span className={styles.Span0}>В</span>
         <span className={styles.Span1}>и</span>
         <span className={styles.Span2}>Я</span>
         <span className={styles.Span3}>н</span>
         <span className={styles.Span4}>а</span>
-      </p>
+      </p> */}
+      <img src={logo}/>
     </div>
   );
 }
